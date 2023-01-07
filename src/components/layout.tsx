e1f9852d76edf8dcd9f45ab1e57de17b3bf2048b
yatrippin <eric.so.dev@gmail.com>
2023-01-06 23:51:25 -0500

    added view project page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import { SessionProvider } from "next-auth/react";
 import { ColorModeProvider } from "../contexts/colorModeContext";
+import { trpc } from "../utils/trpc";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -20,7 +21,7 @@ export default function Layout({ children }: LayoutProps): JSX.Element {
           tabIndex={0}
           className="relative flex h-full w-full flex-col text-neutral-800 dark:bg-black/50 dark:text-indigo-50"
         >
-          <div className="dark:bg-ch-gray-800 absolute top-0 bottom-0 right-0 left-0 -z-10 bg-white/60 backdrop-blur-xl dark:backdrop-blur-0"></div>
+          <div className="absolute top-0 bottom-0 right-0 left-0 -z-10 bg-white/60 backdrop-blur-xl dark:bg-ch-gray-800 dark:backdrop-blur-0"></div>
           <div className="absolute top-0 bottom-0 right-0 left-0 -z-20 bg-gradient-to-br from-green-300 via-blue-500/90 to-purple-600"></div>
           <SessionProvider>
             {" "}
---
+++ src/pages/project/view.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from "next";
+import { trpc } from "../../utils/trpc";
+import { Loading } from "../../components/loading";
+import { Error } from "../../components/error";
+import Link from "next/link";
+
+const ViewProjectPage: NextPage = (req, res) => {
+  const { data: projects, isLoading, isError } = trpc.user.getAllProjects.useQuery();
+
+  if (isError) return <Error></Error>;
+  if (isLoading) return <Loading></Loading>;
+  return (
+    <div className="p-12 xl:px-24 2xl:px-36">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-4">
+        {projects.ownedProjects.map((v) => (
+          <Link href={`/project/${v.slug}`} key={v.id}>
+            <div
+              tabIndex={0}
+              className=" cursor-pointer rounded-md border-[2px] border-white/50 bg-gradient-to-br from-white/40 to-white/10 px-8 py-6 hover:border-white/30 hover:from-white/30 dark:border-gray-800 dark:from-gray-900 dark:to-gray-800/50 dark:hover:border-violet-500/40"
+            >
+              <h2 className="mb-5 text-xl font-semibold text-slate-700 dark:text-slate-300">
+                {v.name}
+              </h2>
+
+              <p className="max-w-xs text-slate-600 dark:text-slate-400">
+                created by <strong className="font-semibold">{v.owner.displayName}</strong>
+              </p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ViewProjectPage;
