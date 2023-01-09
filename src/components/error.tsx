74f6f9b0a0afed0e8da77266fa571c34c78125cb
Eric So <eric.so.dev@gmail.com>
2023-01-09 01:01:55 -0500

    extended screen sizes

--- src/components/error.tsx
+++ src/components/error.tsx
@@ -1,3 +1,19 @@
-export function Error(): JSX.Element {
-  return <div>Error</div>;
+import Link from "next/link";
+
+export function Error(props: React.PropsWithChildren): JSX.Element {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="flex flex-col items-start gap-6">
+        <h1 className="text-xl font-medium text-slate-700 dark:text-purple-100">
+          {props.children || "An unexpected error occurred."}
+        </h1>
+        <Link
+          className="rounded-md bg-slate-50 py-2 px-4 font-medium text-slate-600 dark:bg-purple-200/90 dark:text-purple-900"
+          href="/"
+        >
+          back to safety
+        </Link>
+      </div>
+    </div>
+  );
 }
