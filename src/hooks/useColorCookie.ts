53a9de59e70110c6ab05fc0500b43feeb9bbde54
Eric So <eric.so.dev@gmail.com>
2023-01-16 01:47:22 -0500

    color mode refactor

--- src/hooks/useColorCookie.ts
+++ src/hooks/useColorCookie.ts
@@ -1,24 +1,33 @@
 import { useState, useEffect } from "react";
 
 type ColorMode = "light" | "dark";
+const initialTheme = "dark";
 
 export default function useColorCookie() {
-  const [color, setColor] = useState<"light" | "dark">("light");
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // console.log(localStorage.getItem("tailwind-theme"));
-      const cookie = localStorage.getItem("tailwind-theme");
-      console.log(cookie);
-      if (cookie && cookie in ["light", "dark"]) {
-        setColor(cookie as ColorMode);
-        return;
-      }
-      setColor("light");
-    } else {
-      setColor((color) => color || "light");
+  const [color, setColorState] = useState<"light" | "dark">(() => {
+    if (typeof window === "undefined") {
+      return initialTheme;
     }
-  }, []);
+    const cookie = localStorage.getItem("tailwind-theme");
+    if (cookie && cookie in ["light", "dark"]) {
+      return cookie as ColorMode;
+    }
+    return initialTheme;
+  });
+
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     // console.log(localStorage.getItem("tailwind-theme"));
+  //     const cookie = localStorage.getItem("tailwind-theme");
+  //     if (cookie && cookie in ["light", "dark"]) {
+  //       setColor(cookie as ColorMode);
+  //       return;
+  //     }
+  //     setColor("light");
+  //   } else {
+  //     setColor((color) => color || "light");
+  //   }
+  // }, []);
 
   useEffect(() => {
     if (color === "dark") {
@@ -27,10 +36,19 @@ export default function useColorCookie() {
       document.documentElement.classList.remove("dark");
     }
 
-    if (window !== undefined) {
-      localStorage.setItem("tailwind-theme", color);
-    }
+    // if (window !== undefined) {
+    //   localStorage.setItem("tailwind-theme", color);
+    // }
   }, [color]);
 
-  return { color, setColor };
+  return {
+    color,
+    setColor: (color: ColorMode) => {
+      setColorState(color);
+
+      if (window !== undefined) {
+        localStorage.setItem("tailwind-theme", color);
+      }
+    },
+  };
 }
