fc99db23a476446142281a451877792e513b6c43
Eric So <eric.so.dev@gmail.com>
2023-01-19 16:50:24 -0500

    allow selection removal

--- src/components/ProjectSlugPage/inviteModal/invitedMembers.tsx
+++ src/components/ProjectSlugPage/inviteModal/invitedMembers.tsx
@@ -1,8 +1,9 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
 import { useInvites } from "../../../contexts/inviteContext";
 import { InviteDropdown } from "./inviteDropdown";
 
 export function InvitedMembers(): JSX.Element {
-  const { state: invites } = useInvites();
+  const { state: invites, dispatch } = useInvites();
   return (
     <div>
       <h1 className="mb-2 font-medium text-slate-600">Selected Users</h1>
@@ -16,8 +17,19 @@ export function InvitedMembers(): JSX.Element {
             className="flex flex-row items-center justify-between rounded-md bg-slate-50 py-1.5 px-6 pr-3"
           >
             <h1 className="font-medium text-slate-600">{displayName}</h1>
-            <div>
+            <div className="flex items-center gap-2">
               <InviteDropdown displayName={displayName} currentRole={role}></InviteDropdown>
+              <XMarkIcon
+                onClick={() => {
+                  dispatch({
+                    type: "REMOVE",
+                    payload: {
+                      displayName: displayName,
+                    },
+                  });
+                }}
+                className="h-5 w-5 text-slate-500 hover:scale-125   hover:text-red-400"
+              ></XMarkIcon>
             </div>
           </div>
         ))}
