783f61d3cfddcace996b7cc8c84fd1c74ca901df
Eric So <eric.so.dev@gmail.com>
2023-01-19 23:39:24 -0500

    view and accept or decline invites

--- src/components/Toast/toast.tsx
+++ src/components/Toast/toast.tsx
@@ -4,7 +4,6 @@ import { Fragment } from "react";
 interface Props extends React.PropsWithChildren {
   position?: "tr" | "tl";
   isOpen: boolean;
-  close: () => void;
 }
 export function Toast({ isOpen, children, position = "tr" }: Props): JSX.Element {
   const positionClasses = position === "tr" ? "top-5 right-5" : "top-5 left-5";
@@ -21,7 +20,7 @@ export function Toast({ isOpen, children, position = "tr" }: Props): JSX.Element
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <div className={`absolute z-20 rounded-md px-4 py-2 ${positionClasses}`}>{children}</div>
+          <div className={`absolute z-20 ${positionClasses}`}>{children}</div>
         </Transition>
       )}
     </>
---
+++ src/pages/account/invites.tsx
@@ -0,0 +1,82 @@
+import dayjs from "dayjs";
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+import Head from "next/head";
+import { Error } from "../../components/error";
+import { trpc } from "../../utils/trpc";
+
+const gridSizingClasses =
+  "lg:grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] 2xl:grid-cols-[repeat(auto-fill,minmax(18rem,1fr))] 4xl:grid-cols-[repeat(auto-fill,minmax(20rem,1fr))]";
+
+const AccountPage: NextPage = (req, res) => {
+  const { status } = useSession();
+  const acceptInvite = trpc.user.acceptInvite.useMutation();
+  const declineInvite = trpc.user.deleteInvite.useMutation();
+  const {
+    data: invites,
+    isLoading,
+    isError,
+    refetch,
+  } = trpc.user.getInvites.useQuery(undefined, {
+    enabled: status === "authenticated",
+  });
+  if (status === "loading" || isLoading) return <div></div>;
+  if (status === "unauthenticated") return <div>pls sign in</div>;
+  if (isError) return <Error></Error>;
+  return (
+    <div className="flex flex-col gap-8 p-16 xl:px-36 2xl:px-48">
+      <Head>
+        <title>Invites | PasteWin</title>
+      </Head>
+      <div className="flex h-max flex-row items-end gap-2">
+        <h1 className="max-w-[24ch] truncate text-4xl font-semibold text-slate-800 dark:text-slate-100">
+          Invites
+        </h1>
+      </div>
+      <div className={`${gridSizingClasses} grid`}>
+        {invites.map((invite) => (
+          <div
+            key={invite.projectId + invite.userId}
+            tabIndex={0}
+            className="flex h-full cursor-pointer flex-col gap-8 rounded-md border-[2px] border-white/50 bg-gradient-to-br from-white/40 to-white/10 px-8 py-6 shadow-xl shadow-black/5 hover:border-white/30 hover:from-white/30 dark:border-gray-800 dark:from-gray-900 dark:to-gray-800/50 dark:shadow-slate-800/10 dark:hover:border-violet-500/40"
+          >
+            <h2 className="text-xl font-semibold text-slate-700 dark:text-slate-300">
+              {invite.project.name}
+              <h3 className="mt-2 text-base font-medium text-slate-600 dark:text-slate-400">
+                editor
+              </h3>
+            </h2>
+
+            <div className="flex flex-col gap-2">
+              <p className="mt-auto max-w-xs text-slate-600 dark:text-slate-400">
+                invited by{" "}
+                <strong className="font-semibold">{invite.project.owner.displayName}</strong>
+              </p>
+              <div className="divide-x-2 divide-green-600 self-start">
+                <button
+                  onClick={async () => {
+                    await acceptInvite.mutateAsync({ projectId: invite.projectId });
+                    refetch();
+                  }}
+                  className="rounded-l bg-green-300 px-3 py-1 text-green-800 transition-colors hover:bg-green-400/90"
+                >
+                  accept
+                </button>
+                <button
+                  onClick={async () => {
+                    await declineInvite.mutateAsync({ projectId: invite.projectId });
+                    refetch();
+                  }}
+                  className="rounded-r bg-yellow-200 px-3 py-1 text-yellow-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300/90"
+                >
+                  decline
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default AccountPage;
